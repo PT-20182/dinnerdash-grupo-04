09ee60c6a542002b932fe2aea070a2bc25dccbf2
Pedro Torres <pedrohagatorres@gmail.com>
2018-11-20 15:49:14 -0200

    Adição da função delete, link das páginas e
    refatoração do código.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :current_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
+  end
+
+  def show
+    
   end
 
   def new
@@ -10,14 +16,23 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params)
 
-    redirect_to_dogs_path
-  end
-
-  def show
-    @user = User.find(params[:id])
+    redirect_to user_path(user)
   end
 
   def edit
+    
+  end
+
+  def update
+    @user.update(user_params)
+
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user.destroy
+
+    redirect_to users_path
   end
 
   private
@@ -25,5 +40,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
-  
+
+  def current_user
+    @user = User.find(params[:id])
+  end
+
 end
