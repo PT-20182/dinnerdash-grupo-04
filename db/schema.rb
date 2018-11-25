95a7f7fd77e8a9a9dc66f383cf96ea5713b66d8a
​Gustavo Luigi <gustavolusilo@gmail.com>
2018-11-25 13:47:31 -0200

    Changed admin column from integer to boolean in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_125123) do
+ActiveRecord::Schema.define(version: 2018_11_25_154138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.integer "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -25,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_11_25_125123) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
