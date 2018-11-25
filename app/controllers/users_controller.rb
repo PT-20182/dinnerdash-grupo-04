04f7b64ff9734f8c29f94276b3fc6a6b7b691859
​Gustavo Luigi <gustavolusilo@gmail.com>
2018-11-25 11:03:42 -0200

    Changed some files due to the new and improved project structure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,11 @@
 class UsersController < ApplicationController
-  before_action :current_user, only: [:show, :edit, :update, :destroy]
+  before_action :current_user, only: [:show]
 
-  def index
+  def all
     @users = User.all
   end
 
   def show
-    
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    user = User.create(user_params)
-
-    redirect_to user_path(user)
-  end
-
-  def edit
-    
-  end
-
-  def update
-    @user.update(user_params)
-
-    redirect_to user_path(@user)
-  end
-
-  def destroy
-    @user.destroy
-
-    redirect_to users_path
   end
 
   private
@@ -42,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def current_user
-    @user = User.find(params[:id])
+    @user = User.find(params[:format])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
-  resources :users
+
+  get 'users/all'
+  get 'users/show'
+  
+  get 'home/index'
+  
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
