5405f622c8ed1c64662fc809bec24e7330189381
​Gustavo Luigi <gustavolusilo@gmail.com>
2018-11-25 11:01:51 -0200

    Deleted useless column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_133924) do
+ActiveRecord::Schema.define(version: 2018_11_25_125123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
     t.integer "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
