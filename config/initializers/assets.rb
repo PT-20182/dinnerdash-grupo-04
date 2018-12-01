11a567bdf3cb99d0489966b1b8008cc5abcd6bb4
​Gustavo Luigi <gustavolusilo@gmail.com>
2018-12-01 14:42:04 -0200

    Accessing styles directly from layouts, instead of accessing it from general styles for the entire project

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Adding additional assets for using in some layouts
+Rails.application.config.assets.precompile += %w( meals.css )
