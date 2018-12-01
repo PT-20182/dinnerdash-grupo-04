2e7c8b03d5a3b44b066cbb6adb99a4bb4dc94d4e
​Gustavo Luigi <gustavolusilo@gmail.com>
2018-12-01 16:02:05 -0200

    Refactoring bannerBackoff and footer layouts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,4 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 # Adding additional assets for using in some layouts
-Rails.application.config.assets.precompile += %w( meals.css )
+Rails.application.config.assets.precompile += %w( bannerBackoff.css )
+Rails.application.config.assets.precompile += %w( footer.css )
