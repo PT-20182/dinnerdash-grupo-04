0ab39c69a4675feaae7b800c79464f0149e8193d
​Gustavo Luigi <gustavolusilo@gmail.com>
2018-12-02 15:01:24 -0200

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -15,20 +15,6 @@ ActiveRecord::Schema.define(version: 2018_11_25_154138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.boolean "admin", default: false, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-  
   create_table "meal_categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -45,6 +31,20 @@ ActiveRecord::Schema.define(version: 2018_11_25_154138) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["meal_category_id"], name: "index_meals_on_meal_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
