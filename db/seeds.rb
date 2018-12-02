a98eb9207fa1c01101e94daffa495ec1b06e1617
​Gustavo Luigi <gustavolusilo@gmail.com>
2018-12-02 14:51:57 -0200

    Changing admin password for being easier to access

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: "Admin", email: "admin@dinnerdash.com", password: "plsDoNotHack123", admin: true)
+User.create(name: "Admin", email: "admin@dinnerdash.com", password: "admin123", admin: true)
 User.create(name: "Metal Avenger", email: "m.avenger@metaland.com.br", password: "satanImpera", admin: false)
 User.create(name: "Luigi", email: "supermariobro@nintendo.com", password: "S2daisyS2", admin: false)
 User.create(name: "アキラ", email: "shotarokaneda@toho.co.jp", password: "616B697261", admin: false)
