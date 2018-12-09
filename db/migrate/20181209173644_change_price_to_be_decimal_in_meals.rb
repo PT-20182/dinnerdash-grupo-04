7ae40df2bf5f40dcafb94afe9f74881685097506
Pedro Torres <pedrohagatorres@gmail.com>
2018-12-09 15:44:53 -0200

    Migration to change Meal's 'price' to decimal.
    With an option for rollback.

---
+++ db/migrate/20181209173644_change_price_to_be_decimal_in_meals.rb
@@ -0,0 +1,11 @@
+class ChangePriceToBeDecimalInMeals < ActiveRecord::Migration[5.2]
+  # Using 'up' and 'down' to simplify a rollback
+
+  def up # Rails will use this with a 'db:migrate'
+    change_column :meals, :price, :decimal
+  end
+
+  def down # Rails will use this, instead, with a 'db:rollback'
+    change_column :meals, :price, :string
+  end
+end
