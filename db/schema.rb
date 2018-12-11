57e8c8ebf5520234007b4229a138799e1d6e1586
Pedro Torres <pedrohagatorres@gmail.com>
2018-12-11 13:34:12 -0200

    ActiveStorage's migration and changes to DB.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_001504) do
+ActiveRecord::Schema.define(version: 2018_12_11_140032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,9 +47,11 @@ ActiveRecord::Schema.define(version: 2018_12_11_001504) do
     t.string "description"
     t.decimal "price"
     t.binary "image"
-    t.string "available"
+    t.boolean "available"
+    t.bigint "meal_category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["meal_category_id"], name: "index_meals_on_meal_category_id"
   end
 
   create_table "order_meals", force: :cascade do |t|
@@ -92,5 +94,4 @@ ActiveRecord::Schema.define(version: 2018_12_11_001504) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
