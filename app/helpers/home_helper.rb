77c9725024a5b954cb6c5673c200341ae63e6b3b
leoakira <leobarros@cjr.org.br>
2018-12-12 12:05:14 -0200

    Improved styling of homepage

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,6 @@
 module HomeHelper
     def query_meals(meal_category_name)
-        @meals = Meal.joins(:meal_category).where("meals.meal_category_id = meal_categories.id 
+        @meals = Meal.joins(:meal_category).where("meals.meal_category_id = meal_categories.id
                                                   AND meal_categories.name = ?", meal_category_name)
-      end
+    end
 end
