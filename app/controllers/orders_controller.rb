965d68319fc94d6f6302bf129ae9bff3ed518d98
​Gustavo Luigi <gustavolusilo@gmail.com>
2018-12-12 14:33:00 -0200

    Finished backoffice orders index page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :check_user_admin
+  
+  ORDERS_SIZE = 8
+
   def index
+    @page = (params[:page] || 0).to_i
+    @orders = Order.offset(ORDERS_SIZE * @page).limit(ORDERS_SIZE)
   end
 
   def edit
