f16248470ffd395b8b0828a25330c75c95141cc2
​Gustavo Luigi <gustavolusilo@gmail.com>
2018-12-13 06:42:10 -0200

    Now, orders can be created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,12 @@ class HomeController < ApplicationController
       @meal_categories = MealCategory.all
       @meals = Meal.all
       @items = Meal.find(set_cart)
+
+      @total_price = 0
+
+      @items.each do |meal|
+        @total_price = @total_price + meal.price
+      end
   end
 
   private
